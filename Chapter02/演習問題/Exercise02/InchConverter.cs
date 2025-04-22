3030114bdce9ba90d347b64791292fcf4ee1db38
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-04-22 11:41:46 +0900

    演習問題

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public class InchConverter {
+        //定数 
+        private const double ratio = 0.0254;
+        //インチからメートルを求める
+        public static double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //インチからメートルへの変換
+                for (int inch = 1; inch <= 10; inch++) {
+                    double meter = InchConverter.ToMeter(inch);
+                    Console.WriteLine($"{inch}inch={meter:0.0000}m");
+                }
+            }
+        }
+    }
