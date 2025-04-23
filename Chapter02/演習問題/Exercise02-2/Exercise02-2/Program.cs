fabd6774ef57d49e26241000b8f23b2a198ea852
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-04-23 10:06:10 +0900

    機能完成②

---
+++ Chapter02/演習問題/Exercise02-2/Exercise02-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Exercise022 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+
+            Console.WriteLine("1:ヤードからメートル");
+            Console.WriteLine("2:メートルからヤード");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("はじめの数値:");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("おわりの数値:");
+            int end = int.Parse(Console.ReadLine());
+
+            if (a == 2) {
+                //メートルからヤードへの変換
+                for (int yard = start; yard <= end; yard++) {
+                    double meter = YardConverter.ToYard(yard);
+                    Console.WriteLine($"{yard}yard={meter:0.0000}m");
+                }
+            }
+            if (a == 1) {
+                //ヤードからメートルの変換
+                for (int meter = start; meter <= end; meter++) {
+                    double yard = YardConverter.FromYard(meter);
+                    Console.WriteLine($"{meter}m={yard:0.0000}yard");
+                }
+            }
+        }
+    }
+}
+
---
+++ Chapter02/演習問題/Exercise02-2/Exercise02-2/YardConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise022 {
+    public class YardConverter {
+        //定数 
+        private const double ratio = 1.09361;
+        //メートルからヤードを求める
+        public static double ToYard(double feet) {
+            return feet * ratio;
+        }
+        //ヤードからメートルを求める
+        public static double FromYard(double meter) {
+            return meter / ratio;
+        }
+    }
+}
