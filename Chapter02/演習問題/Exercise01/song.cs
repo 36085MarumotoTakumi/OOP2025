e6f58c1a4c8de59f944b61b01c9701a0757bff10
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-05-13 11:09:55 +0900

    2.1.3まで完了

---
+++ Chapter02/演習問題/Exercise01/song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1 
+    public class Song {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        //2.1.2
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+    }
+}
