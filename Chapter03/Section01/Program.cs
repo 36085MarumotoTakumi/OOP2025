ac3f2fe440a6d6a3a0bc80b10961e5e115134a66
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-05-16 14:17:06 +0900

    Ch301

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -6,15 +6,14 @@
             //int num = int.Parse(Console.ReadLine());
 
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            
-            Judgement judge = isEven;
-            Console.WriteLine(Count(numbers, judge));
+
+            Console.WriteLine(Count(numbers, n => n <= n && n < 10));
         }
         //メソッドへ渡す処理
         static bool isEven(int n) {
             return n % 2 == 0;
         }
-        static int Count(int[] numbers, Judgement judge) {
+        static int Count(int[] numbers, Func<int, bool> judge) {
             var count = 0;
             foreach (var n in numbers) {
                 //引数で受け取ったメソッドを呼び出す
