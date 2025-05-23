dee09eceb7eab89aad01464eb2485628145c97de
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-05-23 15:07:04 +0900

    4-3

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -15,15 +15,39 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            //foreach文
+            foreach (var lang in langs) {
+                if (lang.Contains('S')) {
+                    Console.WriteLine(lang);
+                }
+            }
+            //for文
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S')) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+            //while文
+            int index = 0;
+            while (index < langs.Count) {
+                if (langs[index].Contains('S'))
+                    Console.WriteLine(langs[index]);
+                index++;
+
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-            
+            var selected = langs.Where(x => x.Contains('S'));
+            foreach (var lang in selected) {
+                Console.WriteLine(selected);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-            
+          
+            Console.WriteLine(langs.Find(x => x.Length == 10)?? "Unknown");
+
         }
     }
 }
