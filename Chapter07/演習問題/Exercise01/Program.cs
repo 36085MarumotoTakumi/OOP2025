d6dc77f1bbf2c706e0c1a53b0b74a17643045aba
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-05-27 11:23:30 +0900

    Exercise01完成

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            foreach (var n in numbers.Skip(numbers.Length - 2)) {
+                Console.WriteLine(n);
+            }
+
+        }
+
+        private static void Exercise3(int[] numbers) {
+            for (int i = 0; i < numbers.Count(); i++) {
+                string num = numbers[i].ToString().PadLeft(3, '0');
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void Exercise4(int[] numbers) {
+            foreach(var n in numbers.Order().Take(3) ){
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise5(int[] numbers) {
+            var count = numbers.Distinct().Where(n => n >= 10).Count();
+            Console.WriteLine(count);
+        }
+    }
+}
