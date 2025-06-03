1fda785dfed0c48e5a2429d6b6c16825a74b9af5
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-06-03 10:26:33 +0900

    6.4完成

---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var array = line.Split(';');
+            array[0] = array[0].Replace("Novelist=", "作家　　:");
+            array[1] = array[1].Replace("BestWork=", "代表作　:");
+            array[2] = array[2].Replace("Born=", "誕生年　:");
+
+            foreach (var item in array) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
+
