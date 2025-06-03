b26aff28519d24b28b21769d61eb56c945fca67c
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-06-03 11:35:41 +0900

    Section03追加

---
+++ Chapter06/Section03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Section03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var sb = new StringBuilder();
+            foreach (var word in GetWords()) {
+                sb.Append(word);
+            }
+            var text=sb.ToString();
+            Console.WriteLine(text);
+
+            String str = "";
+            foreach (var word in GetWords()) {
+                str += word;
+            }
+            Console.WriteLine(str);
+        }
+        private static IEnumerable<object> GetWords() {
+            return ["ORange","Lemon","Strawberry"];
+        }
+    }
+}
