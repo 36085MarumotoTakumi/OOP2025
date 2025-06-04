fc6d0bd88e8bc15a668ec40216d08f4b05b1402c
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-06-04 12:10:33 +0900

    Section01追加

---
+++ Chapter05/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var obj = new MySample();
+
+            var newList = obj.MyList.Add(6).RemoveAt(0);
+            obj.MyList.ForEach(n => Console.Write($"n"));
+            Console.WriteLine();
+            newList.ForEach(n => Console.Write($"{n}"));
+            Console.WriteLine();
+        }
+    }
+    class MySample {
+        public ImmutableList<int> MyList { get; private set; }
+
+        public MySample() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList = list.ToImmutableList();
+        }
+    }
+}
+
