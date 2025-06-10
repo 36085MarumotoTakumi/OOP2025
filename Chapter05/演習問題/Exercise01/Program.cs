f8eb68dae768c8e7785fdafcf3b3afe60fd81624
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-06-10 11:58:28 +0900

    Exercise01完成

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -1,11 +1,69 @@
-﻿namespace Exercise01 {
+﻿
+
+using System.Security.Cryptography;
+
+namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-          
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.3");
+            Exercise3(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
 
 
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
 
+        private static void Exercise2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
 
+        private static void Exercise3(YearMonth[] ymCollection) {
+
+            foreach (var ym in ymCollection) {
+                if (ym.Is21Century) {
+                    Console.WriteLine("21世紀");
+                } else {
+                    Console.WriteLine("21世紀ではない");
+                }
+            }
+        }
+        private static void Exercise4(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                if (ym is null) {
+                    Console.WriteLine("21世紀のデータはありません");
+                    break;
+                } else if (ym.Is21Century) {
+                    Console.WriteLine(ym + "は21世紀です");
+                    break;
+                }
+            }
+        }
+
+        private static void Exercise5(YearMonth[] ymCollection) {
+
+            var array = ymCollection.Select(x => x.AddOneMonth())
+                .OrderBy(x => x.Year)
+                .ThenBy(z => z.Month);
+            foreach (var ym in array) {
+                Console.WriteLine(ym);
+            }
         }
     }
 }
--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -5,22 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
-    public class YearMonth(int year, int month) {
+    public record YearMonth(int year, int month) {
         //5.1.1
         public int Year { get; init; } = year;
         public int Month { get; init; } = month;
         //5.1.2
         public bool Is21Century => 2001 <= Year && Year <= 2100;
-        //5.1.3※未完成
+        //5.1.3
         public YearMonth AddOneMonth() {
-            if (Month<13) {
-                return new YearMonth(,);
+            if (Month<=11) {
+                return new YearMonth(Year,Month+1);
             } else {
-                return new YearMonth(,);
+                return new YearMonth(Year+1,1);
             }
         }
-        //5.1.4※未完成
-        public override string ToString() =>
+        //5.1.4
+        public override string ToString() => $"{Year}年"+$"{Month}月";
+            
 
     }
 }
