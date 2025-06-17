114332c832afca7ebc2599bb83aba686f7cf3c1b
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-06-17 11:34:15 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+
+    public class Student {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
+
+        
+        public Student(string name, string subject, int score) {
+            Name = name;
+            Subject = subject;
+            Score = score;
+        }
+    }
+}
+
