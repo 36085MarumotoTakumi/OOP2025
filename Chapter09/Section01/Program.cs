3a965d1b77e62f995a85cb4a21731f902c63e02f
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-06-27 13:47:34 +0900

    27日分

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var culture = new CultureInfo("ja-JP");
+
+            Console.WriteLine("西暦を入力");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("月を入力");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("日を入力");
+            int day = int.Parse(Console.ReadLine());
+
+            var birthday = new DateTime(year, month, day);
+            var modo = new DateTime(2025,1,1);
+            var today = DateTime.Today;
+            string dayOfWeek = birthday.ToString("dddd", culture);
+            TimeSpan dayyy = (today - birthday);
+
+            Console.WriteLine($"{birthday.Year}年の{birthday.Month}月{birthday.Day}日は{dayOfWeek}です");
+            Console.WriteLine("生まれてから" + dayyy + "日目です");
+
+            bool uru = DateTime.IsLeapYear(birthday.Year);
+            if (uru = true) {
+                Console.WriteLine(birthday.Year + "はうるう年です");
+            } else {
+                Console.WriteLine(birthday.Year + "は平年です");
+            }
+            TimeSpan nwd = today - modo;
+            Console.WriteLine("1月1日から"+nwd+"日目です");
+            while (true) {
+                Console.Write("\r" + DateTime.Now);
+            }
+        
+        }
+    }
+}
---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            var time1 = string.Format($"{dateTime:yyyy/MM/dd HH:mm}");
+            Console.WriteLine(time1);
+        }
+
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            var time2 = dateTime.ToString($"{dateTime:yyyy年MM月dd日 HH時mm分}");
+            Console.WriteLine(time2);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culuture = new CultureInfo("ja-JP");
+            culuture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            var datestr = dateTime.ToString("ggyy", culuture);
+            var dateOfWeek = culuture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+
+            var time3 = string.Format($"{datestr}年{dateTime.Month,2}月{dateTime.Day,2}日({dateOfWeek})");
+            Console.WriteLine(time3);
+        }
+    }
+}
