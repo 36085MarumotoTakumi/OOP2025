4c8177c0781ce15ec8bc9d3d2371d370524852b6
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-07-18 10:53:58 +0900

    タイトル表示OK

---
+++ FormApps/RssReader/RssReader/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace RssReader {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tbUrl = new TextBox();
+            btRssGet = new Button();
+            lbTitles = new ListBox();
+            SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            tbUrl.Location = new Point(12, 12);
+            tbUrl.Name = "tbUrl";
+            tbUrl.Size = new Size(435, 23);
+            tbUrl.TabIndex = 0;
+            // 
+            // btRssGet
+            // 
+            btRssGet.Location = new Point(453, 12);
+            btRssGet.Name = "btRssGet";
+            btRssGet.Size = new Size(75, 23);
+            btRssGet.TabIndex = 1;
+            btRssGet.Text = "取得";
+            btRssGet.UseVisualStyleBackColor = true;
+            btRssGet.Click += btRssGet_Click;
+            // 
+            // lbTitles
+            // 
+            lbTitles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbTitles.FormattingEnabled = true;
+            lbTitles.ItemHeight = 15;
+            lbTitles.Location = new Point(12, 41);
+            lbTitles.Name = "lbTitles";
+            lbTitles.Size = new Size(516, 409);
+            lbTitles.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(544, 472);
+            Controls.Add(lbTitles);
+            Controls.Add(btRssGet);
+            Controls.Add(tbUrl);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbUrl;
+        private Button btRssGet;
+        private ListBox lbTitles;
+    }
+}
