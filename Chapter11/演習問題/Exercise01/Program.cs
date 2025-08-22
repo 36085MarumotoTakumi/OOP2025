7ab54a1c0c24c73943c820a46077ee704116eadc
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-08-22 15:59:19 +0900

    22日午後

--- Chapter11/演習問題/Exercise01/Program.cs
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -9,6 +9,7 @@ namespace Exercise01 {
             Console.WriteLine(IsPhoneNumber("090-9111-1234"));
             Console.WriteLine(IsPhoneNumber("080-9111-1234"));
             Console.WriteLine(IsPhoneNumber("070-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("A060-9111-1234"));
             Console.WriteLine(IsPhoneNumber("060-9111-1234"));
 
 
@@ -18,14 +19,17 @@ namespace Exercise01 {
             //bool isMatch = IsPatternText(text, @"$private");
             
             static bool IsPhoneNumber(string telnum) {
-                var regex = new Regex(@"^0(9|8|7)0");
-                if (regex.IsMatch(telnum)) {
-                    Console.Write(telnum+" ");
-                    return true;
-                } else {
-                    Console.Write(telnum+" ");
-                    return false;
-                }
+
+                return Regex.IsMatch(telnum,@"^0[7-9]0-\d{4}-\d{4}$");
+
+                //var regex = new Regex(@"^0(9|8|7)0");
+                //if (regex.IsMatch(telnum)) {
+                //    Console.Write(telnum+" ");
+                //    return true;
+                //} else {
+                //    Console.Write(telnum+" ");
+                //    return false;
+                //}
           
             }
         }
