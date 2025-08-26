34af442788224b17abc99c935e996b85efd720cb
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-08-26 16:00:06 +0900

    26日授業分

---
+++ Chapter11/演習問題/Exercise04/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+            lines[3] = Regex.Replace(lines[3], @"\s*v4.0\s*", "v5.0");
+            File.WriteAllLines("sampleChange.txt", lines);
+        }
+    }
+}
---
+++ Chapter11/演習問題/Exercise05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Exercise05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.html");
+            var sb = new StringBuilder();
+            foreach (var line in lines) {
+                var s = Regex.Replace(line, @"<(!?|/?)\w+", x => x.Value.ToLower());
+                sb.AppendLine(s);
+            }
+            File.WriteAllText("sampleOut.html",sb.ToString());
+        }
+    }
+}
