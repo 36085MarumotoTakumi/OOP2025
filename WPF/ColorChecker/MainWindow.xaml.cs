4d856b6ed21965be090d49702f0904e9446cfcbc
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-09-09 15:57:50 +0900

    9日分

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,24 +19,36 @@ namespace ColorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        MyColor currentColor;
+
         public MainWindow() {
             InitializeComponent();
+            
+            DataContext = GetColorList();
         }
-
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-
-            byte red = (byte)rSlider.Value;
-            byte green = (byte)gSlider.Value;
-            byte blue = (byte)bSlider.Value;
-
-            colorArea.Background = new SolidColorBrush(Color.FromRgb(red, green, blue));
-
+            var myColor = new MyColor() { Color=Color.FromRgb((byte)rSlider.Value,(byte)gSlider.Value, (byte)bSlider.Value)};
+            colorArea.Background = new SolidColorBrush(myColor.Color);
         }
 
         private void stockButton_Button_Click(object sender, RoutedEventArgs e) {
-            Name = "";
+            stockList.Items.Insert(0, currentColor);/***********************************/
         }
 
-      
+        private void colorSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var comboSelectMyColor = (MyColor)((ComboBox)sender).SelectedItem;
+            setSliderValue(comboSelectMyColor.Color);
+          
+        }
+
+        private void setSliderValue(Color color) {
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
+        }
     }
 }
