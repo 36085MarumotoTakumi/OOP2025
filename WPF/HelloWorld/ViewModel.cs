7b39889176ca21bf20d513474de5360c8297bc6a
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-10-02 15:46:12 +0900

    IsEnable属性

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -3,31 +3,44 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace HelloWorld {
-    class ViewModel : INotifyPropertyChanged {
+    class ViewModel : BindableBase {
         public ViewModel() {
-            ChangeMessageCommand = new DelegateCommand(
-                () => GreetingMessage = "Fuck you");
+            ChangeMessageCommand = new DelegateCommand<string>(
+                (par) => GreetingMessage = par);
         }
 
         private string _greetingMessage = "Hello World";
         public string GreetingMessage {
             get => _greetingMessage;
             set {
-                if (_greetingMessage != value) {
-                    _greetingMessage = value;
-                    PropertyChanged?.Invoke(
-                         this, new PropertyChangedEventArgs(nameof(GreetingMessage)));
+                if (SetProperty(ref _greetingMessage, value)) {
+                    CanChangeMessage = false;
                 }
             }
+            //    {
+            //    if (_greetingMessage != value) {
+            //        _greetingMessage = value;
+            //        PropertyChanged?.Invoke(
+            //             this, new PropertyChangedEventArgs(nameof(GreetingMessage)));
+            //    }
+            //}
         }
-        public DelegateCommand ChangeMessageCommand { get; }
+        private bool _canChangeMessage = true;
+        public bool CanChangeMessage {
+            get => _canChangeMessage;
+            private set => SetProperty(ref _canChangeMessage, value);
+        }
+        public string NewMessage1 { get; } = "I will kill you";
+        public string NewMessage2 { get; } = "You asshole";
+        public DelegateCommand<string> ChangeMessageCommand { get; }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        //public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
