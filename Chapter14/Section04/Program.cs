b48e72d811b41766b29b8989f599a7dd06c50bcb
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-11-05 11:16:37 +0900

    Code 14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient client = new HttpClient();
+            await GetHtmlExample(client);
+        }
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://www.debian.or.jp/";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
