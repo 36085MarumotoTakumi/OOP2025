13bdc2ced60cdffad43ebd2b5808614c17a4f0d8
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-11-10 15:59:47 +0900

    11/10

---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var FilePath = @"./Program.cs";
+            using (var reader = new System.IO.StreamReader(FilePath)) {
+                string line;
+                int cnt = 1;
+                if (true) {
+                    cnt = cnt + 1;
+                }
+            }
+
+        }
+    }
+}
