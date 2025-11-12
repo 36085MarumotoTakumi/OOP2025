def631b483d0b7d34e8a6549a8662e7d13712461
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-11-12 10:12:34 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<GreetingBase> list = [
+                new GrretingMorning(),
+                new GrretingAfternoon(),
+                new GrretingEvening(),
+                ];
+            foreach (var obj in list) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+               
+            }
+        }
+    }
+    class GrretingMorning:GreetingBase {
+        public override string GetMessage()=> "おはよう";
+    }
+    class GrretingAfternoon:GreetingBase {
+        public override string GetMessage() => "こんにちは";
+    }
+    class GrretingEvening:GreetingBase {
+        public override string GetMessage() => "こんばんは";
+    }
+}
