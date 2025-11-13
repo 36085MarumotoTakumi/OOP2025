54a917b9712e7f22c187aef750aa5ba674930735
MarumotoTakumi <oibc36085@ojs.ac.jp>
2025-11-13 13:26:56 +0900

    LineCounter

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -5,7 +5,8 @@ namespace LineCounter {
     internal class Program {
         
         static void Main(string[] args){
-       
+            string path=@"C:\\Users\\infosys\\source\\repos\\OOP2025\\Chapter06\\Section03\\Program.cs";
+            TextProcessor.Run<LineCounterProcessor>(path);
             
         }
     }
